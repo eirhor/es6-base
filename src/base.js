394a1edcf081143c2c808248859c5b9ecce61b8d
Eirik Horvath <eirik@geta.no>
2017-07-29 17:46:49 +0200

    Added base class

---
+++ src/base.js
@@ -0,0 +1,15 @@
+class Base {
+    constructor() {
+        if (document.readyState === 'interactive' || document.readyState === 'complete') {
+            return this.init();
+        }
+
+        document.addEventListener('DOMContentLoaded', this.init(), false);
+    }
+
+    init() {
+        console.warn('Override the init function when extending this class.') // eslint-disable-line
+    }
+}
+
+export default Base;
